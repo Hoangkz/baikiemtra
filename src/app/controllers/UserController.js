1ec285d5015f920c610611951004f00a6d76b074
hoang <hoangyl245@gmail.com>
2022-11-25 00:51:10 +0700

    update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -8,7 +8,7 @@ class UserController{
         User.find({})
             // res.json(req.params.id)
             .then(user => {
-                return res.render('user/LishUser',{
+                return res.render('user/lishuser',{
                     user: mutipleMongooseToObject(user),
                     data: res.data
                 });
